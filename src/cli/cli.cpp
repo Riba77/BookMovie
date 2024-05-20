322ea2dfc313eba5ca8af3e3dcb7688ae7cea473
Roman <roman.hribar@lumentum>
2024-05-20 17:45:34 +0200

    Add cli test in main.

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -2,12 +2,26 @@
 #include <iostream>
 #include "cinemaplex.h"
 
-using namespace cpx;
-
-int main(int argc, char const *argv[]) {
+int main(int argc, char const* argv[]) {
   // Main function
-  TheaterId theaterId("InitCommit");  // Call the constructor
-  std::cout << "Theater ID: " << theaterId.id
-            << std::endl;  // Print the theater ID
+  CTheater theater("01", 10);  // Call the constructor
+  theater.AddShow(SShowParams(1, "01", "10:00", "Theater1"));  // Add a show
+  theater.AddShow(SShowParams(2, "02", "12:00", "Theater2"));  // Add a show
+  theater.AddShow(SShowParams(3, "03", "14:00", "Theater3"));  // Add a show
+  std::cout << "Theater ID: " << theater.Id
+            << std::endl;            // Print the theater ID
+  auto result = theater.GetShow(2);  // Get the show
+  if (result.has_value()) {
+    CShow& show = result.value();  // Get the show
+    std::cout << "Show Time: " << show.ShowParams.Time
+              << std::endl;  // Print the show ID
+    std::cout << "Movie Id: " << show.ShowParams.MovieId
+              << std::endl;  // Print the show time
+    std::cout << ""
+                 "Theater Id: "
+              << show.ShowParams.TheaterId << std::endl;  // Print the movie ID
+  } else {
+    std::cout << "Show not found" << std::endl;  // Print show not found
+  }
   return 0;
 }
